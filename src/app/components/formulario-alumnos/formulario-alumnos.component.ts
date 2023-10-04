dd9da8db5cedc1d5d1f246f78f71ce9b4d10539d
camilo3900 <camilogv.1629@gmail.com>
2023-10-04 13:24:38 -0500

    vinculacion de bootstrap al proyecto

---
+++ src/app/components/formulario-alumnos/formulario-alumnos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-alumnos',
+  templateUrl: './formulario-alumnos.component.html',
+  styleUrls: ['./formulario-alumnos.component.scss']
+})
+export class FormularioAlumnosComponent {
+
+}
